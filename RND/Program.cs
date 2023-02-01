c95c88d7c2efb0fe73c41b0e6f0ed7ce56161148
torst <torst@BORSTEN>
2023-02-01 16:24:10 +0100

    Projektdateien hinzufügen.

---
+++ RND/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RND
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = new int[1000];
+            int rnd = 0;
+            Random random = new Random();
+            for (int i = 0; i < 1000000; i++)
+            {
+
+                rnd = random.Next(0, 1000);
+                ints[rnd] = ints[rnd] + 1;
+            }
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.WriteLine(i + " wurde " + ints[i] + " mal generiert!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
